421bea256ccfe4aeb5a627acb3750cc3f8706c5e
Jean T <jtissot@flowbe.fr>
2024-01-24 10:28:08 +0100

    Fix for long paths on windows

--- merger.py
+++ merger.py
@@ -1,12 +1,12 @@
 from pypdf import PdfMerger
 import os
 
-pdfToAppend = "./append.pdf"
-pdfDirectory = "./input"
-outputDirectoryPrefix = "./output"
+pdfToAppend = "append.pdf"
+pdfDirectory = ".\\input"
+outputDirectoryPrefix = ".\\output"
 
 # check if input folder exists
-if (not os.path.exists(pdfDirectory)):
+if not os.path.exists(pdfDirectory):
     print("input folder does not exist. Exiting...")
     exit(1)
 
@@ -18,13 +18,27 @@ while os.path.exists(outputDirectory):
     outputDirectory = outputDirectoryPrefix + "-" + str(suffix)
 os.mkdir(outputDirectory)
 
+
+def get_long_path(dirpath, filename):
+    path = os.path.abspath(os.path.join(dirpath, filename))
+    if path.startswith("\\\\"):
+        path = "\\\\?\\UNC\\" + path[2:]
+    else:
+        path = "\\\\?\\" + path
+    return path
+
+
+pathPdfToAppend = get_long_path(".\\", pdfToAppend)
+
 # merge pdfs
 for dirpath, dirnames, filenames in os.walk(pdfDirectory):
     for filename in filenames:
-        if (filename.endswith(".pdf")):
+        if filename.endswith(".pdf"):
             print("Appending a page to " + filename)
+            input_file_path = get_long_path(dirpath, filename)
+            output_file_path = get_long_path(outputDirectory, filename)
             merger = PdfMerger()
-            merger.append(pdfDirectory + "/" + filename)
-            merger.append(pdfToAppend)
-            merger.write(outputDirectory + "/" + filename)
+            merger.append(input_file_path)
+            merger.append(pathPdfToAppend)
+            merger.write(output_file_path)
             merger.close()
